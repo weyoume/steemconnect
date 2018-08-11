ccc17c5136d76844064f8d13051938f621ae2bf2
lopu <lopudesigns@gmail.com>
2018-08-11 10:23:06 +0000

    ready

--- db/models/index.js
+++ db/models/index.js
@@ -6,8 +6,8 @@ const basename = path.basename(module.filename);
 const config = require(`${__dirname}/../config/config.json`);
 const logger = require(`${__dirname}/../../helpers/logger`).child({ module: 'db' });
 const db = {};
-const sequelize = new Sequelize(process.env[config.use_env_variable], config);
 var dotenv = require('dotenv')()
+const sequelize = new Sequelize(process.env[config.use_env_variable], config);
 
 config.logging = function (msg) {
   logger.debug(msg);
