04b5cc74c28ff4492e6303ff910ce62321e73830
lopu <ireply@myleisure.com.au>
2018-08-12 02:21:52 +1000

    ready

--- scripts/install_postgresql_yum.sh
+++ scripts/install_postgresql_yum.sh
@@ -8,8 +8,10 @@
 echo "*****************************************"
 echo " Installing PostgreSQL"
 echo "*****************************************"
-sudo yum -y install postgresql postgresql-server postgresql-devel postgresql-contrib postgresql-docs
-sudo service postgresql initdb
+sudo yum -y install postgresql94 postgresql94-server
+/usr/pgsql-9.4/bin/postgresql94-setup initdb
+
+# sudo service postgresql initdb
 # Use MD5 Authentication
 sudo sed -i.bak -e 's/ident$/md5/' -e 's/peer$/md5/' /var/lib/pgsql9/data/pg_hba.conf
 #start
