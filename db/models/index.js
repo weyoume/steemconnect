dadcba982fa0d0b4b6c2733278b68c028975218d
lopu <ireply@myleisure.com.au>
2018-08-11 20:30:54 +1000

    ready

--- db/models/index.js
+++ db/models/index.js
@@ -7,6 +7,7 @@ const config = require(`${__dirname}/../config/config.json`);
 const logger = require(`${__dirname}/../../helpers/logger`).child({ module: 'db' });
 const db = {};
 var dotenv = require('dotenv')()
+console.log(process.env)
 const sequelize = new Sequelize(process.env[config.dburl], config);
 
 config.logging = function (msg) {
