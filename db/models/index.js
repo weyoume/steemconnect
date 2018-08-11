3d303413d41e19188016ef3e0ee232841c3883e9
lopu <ireply@myleisure.com.au>
2018-08-11 21:11:22 +1000

    ready

--- db/models/index.js
+++ db/models/index.js
@@ -8,7 +8,21 @@ const logger = require(`${__dirname}/../../helpers/logger`).child({ module: 'db'
 const db = {};
 var dotenv = require('dotenv')()
 console.log(process.env)
-const sequelize = new Sequelize(process.env[config.dburl], config);
+const sequelize = new Sequelize(process.env[config.dburl], {
+  "dburl": process.env[config.dburl],
+	"logging": false,
+	"dialect" : "postgres",
+  "dialectOptions": {
+		"ssl": false
+	},
+  "operatorsAliases": false,
+  "pool": {
+    "max": 150,
+    "min": 0,
+    "idle": 10000
+	},
+	"port":5432
+});
 config.logging = function (msg) {
   logger.debug(msg);
 };
