836f9873dbf6cac64cb894a8e6b0b475500e8811
lopu <ireply@myleisure.com.au>
2018-08-14 17:26:43 +1000

    ready

--- scripts/install_postgresql_yum.sh
+++ scripts/install_postgresql_yum.sh
@@ -2,8 +2,8 @@
 ###############################################
 # To use: 
 # https://raw.github.com/gist/2776351/???
-# chmod 777 install_postgresql.sh
-# ./install_postgresql.sh
+# chmod 777 install_postgresql_yum.sh
+# ./install_postgresql_yum.sh
 ###############################################
 echo "*****************************************"
 echo " Installing PostgreSQL"
@@ -13,8 +13,8 @@ sudo service postgresql94 initdb
 # Use MD5 Authentication
 sudo sed -i.bak -e 's/ident$/md5/' -e 's/peer$/md5/' /var/lib/pgsql94/data/pg_hba.conf
 #start
-sudo service postgresql94 start
 # sudo /sbin/chkconfig --levels 235 postgresql on
+sudo service postgresql94 start
 
 # http://imperialwicket.com/aws-install-postgresql-on-amazon-linux-quick-and-dirty
 
